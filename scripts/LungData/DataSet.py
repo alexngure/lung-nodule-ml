fca81cd48f03b130db9a5ce6bd4e471ca3f08ffc
Alex Ngure <ngure001@umn.edu>
2017-03-27 21:42:53 -0500

    Fix a typo -- 'Dataset' shouldn't be written in camelcase.

--- scripts/LungData/DataSet.py
+++ scripts/LungData/DataSet.py
@@ -4,21 +4,21 @@
 
 import flatbuffers
 
-class DataSet(object):
+class Dataset(object):
     __slots__ = ['_tab']
 
     @classmethod
-    def GetRootAsDataSet(cls, buf, offset):
+    def GetRootAsDataset(cls, buf, offset):
         n = flatbuffers.encode.Get(flatbuffers.packer.uoffset, buf, offset)
-        x = DataSet()
+        x = Dataset()
         x.Init(buf, n + offset)
         return x
 
-    # DataSet
+    # Dataset
     def Init(self, buf, pos):
         self._tab = flatbuffers.table.Table(buf, pos)
 
-    # DataSet
+    # Dataset
     def Data(self, j):
         o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(4))
         if o != 0:
@@ -31,14 +31,14 @@ class DataSet(object):
             return obj
         return None
 
-    # DataSet
+    # Dataset
     def DataLength(self):
         o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(4))
         if o != 0:
             return self._tab.VectorLen(o)
         return 0
 
-def DataSetStart(builder): builder.StartObject(1)
-def DataSetAddData(builder, data): builder.PrependUOffsetTRelativeSlot(0, flatbuffers.number_types.UOffsetTFlags.py_type(data), 0)
-def DataSetStartDataVector(builder, numElems): return builder.StartVector(4, numElems, 4)
-def DataSetEnd(builder): return builder.EndObject()
+def DatasetStart(builder): builder.StartObject(1)
+def DatasetAddData(builder, data): builder.PrependUOffsetTRelativeSlot(0, flatbuffers.number_types.UOffsetTFlags.py_type(data), 0)
+def DatasetStartDataVector(builder, numElems): return builder.StartVector(4, numElems, 4)
+def DatasetEnd(builder): return builder.EndObject()
