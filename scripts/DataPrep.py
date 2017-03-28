3577504de152d2121f618fda1f9ec159231fa109
Alex Ngure <ngure001@umn.edu>
2017-03-27 21:46:51 -0500

    Add a data prep script to package all nodule data into a single binary file.

---
+++ scripts/DataPrep.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import json
+
+import flatbuffers
+import LungData.Point as FBPoint
+import LungData.RegionOfInterest as FBROI
+import LungData.Nodule as FBNodule
+import LungData.Dataset as FBDataset
+
+from Utils import extract_nodules, outline_nodule, print_image
+
+LIDC_XML_DIR = ""
+DCM_FILE_MAP_JSON = ""
+RAW_IMAGE_DIR = ""
+PROCESSED_IMAGE_DIR = ""
+BIN_DEST = ""
+
+
+
