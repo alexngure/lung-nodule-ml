b84cbf88c8a573baddeec76e8eee4645ba4310ff
Alex Ngure <ngure001@umn.edu>
2017-04-02 14:31:04 -0500

    Add a method to convert the non-flatbuffers Nodule class into a buffer.

--- scripts/DataPrep.py
+++ scripts/DataPrep.py
@@ -29,5 +29,33 @@ def create_FBROI(builder,my_ROI):
     ROI = FBROI.RegionOfInterestEnd(builder)
     return ROI
 
+def create_FBNodule(builder,my_nodule):
+    """Takes an instance of the non-flatbuffers implementation of the Nodule class and
+    converts it into a buffer.
+    """
+    ROI_list = []
+    for item in my_nodule.ROI_list():
+        ROI_list.append(create_FBROI(builder,item))
+    FBNodule.NoduleStartROIVector(builder,len(ROI_list))
+    for roi in ROI_list:
+        builder.PrependUOffsetTRelative(roi)
+    ROI_vec = builder.EndVector(len(ROI_list))
+    nodule_ID = builder.CreateString(my_nodule.noduleID())
+    FBNodule.NoduleStart(builder)
+    FBNodule.NoduleAddNoduleId(builder,nodule_ID)
+    characteristics = my_nodule.characteristics()
+    if characteristics is not None:
+        FBNodule.NoduleAddSubtlety(builder,characteristics._subtlety)
+        FBNodule.NoduleAddInternalStructure(builder,characteristics._internal_structure)
+        FBNodule.NoduleAddCalcification(builder,characteristics._calcification)
+        FBNodule.NoduleAddSphericity(builder,characteristics._sphericity)
+        FBNodule.NoduleAddMargin(builder,characteristics._margin)
+        FBNodule.NoduleAddLobulation(builder,characteristics._lobulation)
+        FBNodule.NoduleAddSpiculation(builder,characteristics._spiculation)
+        FBNodule.NoduleAddTexture(builder,characteristics._texture)
+        FBNodule.NoduleAddMalignancy(builder,characteristics._malignancy)
+    FBNodule.NoduleAddROI(builder,ROI_vec)
+    nodule = FBNodule.NoduleEnd(builder)
+    return nodule
 
 
