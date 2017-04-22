ad9d009a45f95df508604f7e8b8eeebdd6058419
alexngure <ngure001@umn.edu>
2017-04-22 05:13:31 -0700

    Second convolution layer: computes 64 features for a 5x5 area of an image.

--- tensorflow/deep_mnist.py
+++ tensorflow/deep_mnist.py
@@ -23,3 +23,7 @@ x_image = tf.reshape(x,[-1,28,28,1])
 
 h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
+
+W_conv2 = weight_variable([5,5,32,64])
+b_conv2 = bias_variable([64])
+
