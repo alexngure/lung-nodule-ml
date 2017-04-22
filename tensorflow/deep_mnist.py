8a4e5b911842a7ea6ddfb839cc123ba70d616cbd
alexngure <ngure001@umn.edu>
2017-04-22 05:22:13 -0700

    Add output layer.

--- tensorflow/deep_mnist.py
+++ tensorflow/deep_mnist.py
@@ -38,3 +38,8 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
+
+W_fc2 = weight_variable([1024,10])
+b_fc2 = bias_variable([10])
+
+y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
