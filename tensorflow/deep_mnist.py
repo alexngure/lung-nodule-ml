0309a2876cfe469cf8ab17b0344e9fde87c22dea
alexngure <ngure001@umn.edu>
2017-04-22 05:13:48 -0700

    Convolve and max pool.

--- tensorflow/deep_mnist.py
+++ tensorflow/deep_mnist.py
@@ -27,3 +27,6 @@ h_pool1 = max_pool_2x2(h_conv1)
 W_conv2 = weight_variable([5,5,32,64])
 b_conv2 = bias_variable([64])
 
+h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
