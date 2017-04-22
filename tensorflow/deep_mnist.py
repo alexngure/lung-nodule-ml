103ec2ccda576486560a18c952f3f291080ada69
alexngure <ngure001@umn.edu>
2017-04-22 05:21:00 -0700

    Dropout -- keep_prob = 0.5 during training, 1.0 during testing.

--- tensorflow/deep_mnist.py
+++ tensorflow/deep_mnist.py
@@ -37,3 +37,4 @@ h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
