646a88951f44ee93c4b08e356ff98a689c349538
alexngure <ngure001@umn.edu>
2017-04-22 05:29:37 -0700

    Run 20,000 iterations, training on batches of 50 random data points in each run.

--- tensorflow/deep_mnist.py
+++ tensorflow/deep_mnist.py
@@ -49,3 +49,11 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
+for i in range(20000):
+    batch = mnist.train.next_batch(50)
+    if i%100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_:batch[1], keep_prob=1.0})
+        print("Sted %d, training accuracy %g"%(i,train_accuracy))
+    train_step.run(feed_dict={x:batch[0], y_:batch[1], keep_prob = 0.5})
+
+print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images,y_:mnist.test.labels, keep_prob:1.0}))
