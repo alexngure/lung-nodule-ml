0967b849f71b0630b132e368735dbada5938e00f
alexngure <ngure001@umn.edu>
2017-04-23 03:59:22 -0700

    Train the network, printing the accuracy every 100 iterations.

--- tensorflow/deep_mnist.py
+++ tensorflow/deep_mnist.py
@@ -52,4 +52,23 @@ def main():
     y_ = tf.placeholder(tf.float32, [None,10])
     y_conv,keep_prob = deepnn(x)
     
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+    
+    with tf.Session() as sesh:
+        sesh.run(tf.global_variables_initializer())
+        for i in range(20000):
+            batch = mnist.train.next_batch(50)
+            if i%100 == 0:
+                train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_:batch[1], keep_prob:1.0})
+                print("Step %d, training accuracy %g"%(i,train_accuracy))
+            train_step.run(feed_dict={x:batch[0], y_:batch[1], keep_prob:0.5})
+        
+        print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images,y_:mnist.test.labels, keep_prob:1.0}))
+        
+if __name__ == '__main__':
+    print('Commencing training...')
+    main()
     
