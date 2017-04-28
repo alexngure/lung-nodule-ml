50fd1d6145b32383eea3965329732dc15ec06eb0
alexngure <ngure001@umn.edu>
2017-04-27 20:53:15 -0500

    Placeholders for the label predicted and actual labels.

--- model/simple_lung.py
+++ model/simple_lung.py
@@ -3,3 +3,7 @@ import tensorflow as tf
 x = tf.placeholder(tf.float32, [None,512*512])
 W = tf.Variable(tf.zeros([512*512,2]))
 b = tf.Variable(tf.zeros([2]))
+
+y = tf.matmul(x,W) + b
+y_ = tf.placeholder(tf.float32, [None,2])
+
