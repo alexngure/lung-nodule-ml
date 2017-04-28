eb0469458e5b05533c590f773b59605189883383
alexngure <ngure001@umn.edu>
2017-04-27 20:53:41 -0500

    Add loss function.

--- model/simple_lung.py
+++ model/simple_lung.py
@@ -7,3 +7,4 @@ b = tf.Variable(tf.zeros([2]))
 y = tf.matmul(x,W) + b
 y_ = tf.placeholder(tf.float32, [None,2])
 
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
