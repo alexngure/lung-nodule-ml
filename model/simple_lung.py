ae1073f3557f11bdb2d55aa5f57c80ba734e18b0
alexngure <ngure001@umn.edu>
2017-04-27 20:54:49 -0500

    Use simple Gradient Descent with learning rate of  0.5.

--- model/simple_lung.py
+++ model/simple_lung.py
@@ -8,3 +8,4 @@ y = tf.matmul(x,W) + b
 y_ = tf.placeholder(tf.float32, [None,2])
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+train_step = tf.nn.GradientDescentOptimizer(0.5).minimize(cross_entropy)
