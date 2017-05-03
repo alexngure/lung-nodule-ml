6675db61d95c00a1269dfe880f790cfbb7634499
alexngure <ngure001@umn.edu>
2017-05-02 21:39:46 -0500

    A class that loads '.data' and exposes convenient accessors.

--- data/DataManager.py
+++ data/DataManager.py
@@ -2,3 +2,17 @@
 import TrainingData.Example as FBExample
 import TrainingData.Header as FBHeader
 import TrainingData.TrainingSet as FBTrainingSet
+
+class DataLoader(object):
+    """A class to facilitate data loading during training and testing."""
+    def __init__(self, path, train_percent = 0.8):
+        self.path = ''
+        self.test_images = ''
+        self.test_labels = ''
+        self.train_percent = train_percent
+        self.test_percent = 1 - train_percent
+    def load_data(self):
+        return
+    def next_batch(self,batch_size):
+        return
+
