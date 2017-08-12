7cc469b632d7152132cae7ab6699c6355b97eeb8
alexngure <ngure001@umn.edu>
2017-08-12 15:05:25 -0500

    Allow specification of strides for convolutions and max pooling.

--- model/deep_lung.py
+++ model/deep_lung.py
@@ -67,21 +67,15 @@ def lung_cnn(x):
     y_conv = tf.matmul(h_fc1_drop,w_fc2) + b_fc2
     return y_conv
 
-def conv2d(x, W):
-  """conv2d returns a 2d convolution layer with full stride.
-
-  source: tensorflow/deep_mnist.py
-  """
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def conv2d(x, W,strides=[1,1,1,1]):
+  """conv2d returns a 2d convolution layer with specified stride."""
+  return tf.nn.conv2d(x, W, strides=strides, padding='SAME')
 
 
-def max_pool_2x2(x):
-  """max_pool_2x2 downsamples a feature map by 2X.
-
-  source: tensorflow/deep_mnist.py
-  """
+def max_pool_2x2(x,strides=[1,2,2,1]):
+  """max_pool_2x2 downsamples a feature map by 2X."""
   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
+                        strides=strides, padding='SAME')
 
 
 def weight_variable(shape,name):
