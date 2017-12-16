a3634775bc1873dc4d54e5225e22838dea5a8c34
alexngure <ngure001@umn.edu>
2017-12-15 23:07:38 -0600

    Update net architecture.

--- model/deep_lung.py
+++ model/deep_lung.py
@@ -41,50 +41,51 @@ def lung_cnn(x):
     """A deep net that identifies nodules in a 2D patch of
     a lung image.
     """
-    W_conv1 = weight_variable([3,3,1,32],name='W_conv1')
-    b_conv1 = bias_variable([32],name='b_conv1')
-    h_conv1 = tf.nn.relu(conv2d(x,W_conv1) + b_conv1)
+    x_image = tf.reshape(x,[-1,INPUT_SHAPE[0],INPUT_SHAPE[1],1])
+    W_conv1 = weight_variable([3,3,1,64],name='W_conv1')
+    b_conv1 = bias_variable([64],name='b_conv1')
+    h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1) + b_conv1)
 
     # downsample to 32x32
     h_pool1 = max_pool_2x2(h_conv1)
 
-    W_conv2 = weight_variable([3,3,32,64],name='W_conv2')
-    b_conv2 = bias_variable([64],name='b_conv2')
+    W_conv2 = weight_variable([3,3,64,128],name='W_conv2')
+    b_conv2 = bias_variable([128],name='b_conv2')
     h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2) + b_conv2)
 
     # downsample to 16x16
     h_pool2 = max_pool_2x2(h_conv2)
 
-    W_conv3 = weight_variable([3,3,64,128],name='W_conv3')
-    b_conv3 = bias_variable([128],name='b_conv3')
-    h_conv3 = tf.nn.relu(conv2d(h_pool2,W_conv2) + b_conv2)
+    W_conv3 = weight_variable([3,3,128,256],name='W_conv3')
+    b_conv3 = bias_variable([256],name='b_conv3')
+    h_conv3 = tf.nn.relu(conv2d(h_pool2,W_conv3) + b_conv3)
 
     # downsample to 8x8
     h_pool3 = max_pool_2x2(h_conv3)
 
-    W_conv4 = weight_variable([3,3,128,64],name='W_conv4')
-    b_conv4 = bias_variable([64],name='b_conv4')
-    h_conv4 = tf.nn.relu(conv2d(h_pool3,W_conv2) + b_conv2)
+    W_conv4 = weight_variable([3,3,256,128],name='W_conv4')
+    b_conv4 = bias_variable([128],name='b_conv4')
+    h_conv4 = tf.nn.relu(conv2d(h_pool3,W_conv4) + b_conv4)
 
     # downsample to 4x4
     h_pool4 = max_pool_2x2(h_conv4)
 
-    W_conv5 = weight_variable([3,3,64,32],name='W_conv5')
-    b_conv5 = bias_variable([32],name='b_conv5')
-    h_conv5 = tf.nn.relu(conv2d(h_pool4,W_conv2) + b_conv2)
+    W_conv5 = weight_variable([3,3,128,64],name='W_conv5')
+    b_conv5 = bias_variable([64],name='b_conv5')
+    h_conv5 = tf.nn.relu(conv2d(h_pool4,W_conv5) + b_conv5)
 
     # downsample to 2x2
     h_pool5 = max_pool_2x2(h_conv5)
 
-    W_conv6 = weight_variable([3,3,32,16],name='W_conv6')
-    b_conv6 = bias_variable([16],name='b_conv6')
-    h_conv6 = tf.nn.relu(conv2d(h_pool5,W_conv2) + b_conv2)
+    W_conv6 = weight_variable([3,3,64,32],name='W_conv6')
+    b_conv6 = bias_variable([32],name='b_conv6')
+    h_conv6 = tf.nn.relu(conv2d(h_pool5,W_conv6) + b_conv6)
 
     # downsample to 1x1
     h_pool6 = max_pool_2x2(h_conv6)
-    h_pool6_flat = tf.reshape([-1,16])
+    h_pool6_flat = tf.reshape(h_pool6,[-1,32])
 
-    W_fc1 = weight_variable([16,1024],name='W_fc1')
+    W_fc1 = weight_variable([32,1024],name='W_fc1')
     b_fc1 = bias_variable([1024],name='b_fc1')
     h_fc1 = tf.nn.relu(tf.matmul(h_pool6_flat,W_fc1) + b_fc1)
 
@@ -94,8 +95,8 @@ def lung_cnn(x):
     W_fc2 = weight_variable([1024,NUM_CLASSES],name='W_fc2')
     b_fc2 = bias_variable([NUM_CLASSES],name='b_fc2')
 
-    y_conv = tf.matmul(h_fc1_drop,w_fc2) + b_fc2
-    return y_conv
+    y_conv = tf.matmul(h_fc1_drop,W_fc2) + b_fc2
+    return y_conv,keep_prob
 
 def conv2d(x, W,strides=[1,1,1,1]):
     """conv2d returns a 2d convolution layer with specified stride."""
